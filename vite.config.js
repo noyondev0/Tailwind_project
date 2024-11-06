d905c1a62447a627063b8f340eb9621e0b634058
noyondev0 <mdnoyonss171@gmail.com>
2024-11-06 20:57:24 +0600

    vite config update

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            main: resolve(__dirname, 'index.html'),
+            news: resolve(__dirname, 'news.html')
+        }
+    }
+})
